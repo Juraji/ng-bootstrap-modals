a1a11a37dd807be68d6eeeec66186b9c4f96b963
Juraji <github@juraji.nl>
2021-04-19 21:32:14 +0200

    Oops, forgot to export the shiny new directive as library api

--- projects/modals/src/public-api.ts
+++ projects/modals/src/public-api.ts
@@ -7,6 +7,7 @@ export {ModalsModule} from './lib/modals.module';
 export {AutoFocusDirective} from './lib/components/auto-focus.directive';
 export {ResolveDirective} from './lib/components/resolve.directive';
 export {DismissDirective} from './lib/components/dismiss.directive';
+export {ModalFormDirective} from './lib/components/modal-form.directive';
 export {DEFAULT_MODAL_CONFIG, ModalConfig} from './lib/configuration/modal-config';
 export {MODAL_DATA} from './lib/util/modal-data';
 export {ModalRef} from './lib/util/modal-ref';
